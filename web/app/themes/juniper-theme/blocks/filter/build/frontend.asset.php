773ca6cabc6bf61c3d785238d9e8a4e053b640cd
Tino Geißler <tinogeissler@proton.me>
2024-07-03 10:22:41 +0200

    <chore>(filter): build

--- web/app/themes/juniper-theme/blocks/filter/build/frontend.asset.php
+++ web/app/themes/juniper-theme/blocks/filter/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '24d91977c67af8005385');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => 'e3d21f4b2c887c7d3ad0');
