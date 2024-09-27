ea56ef170d9434f0a2788181101ee5ce12dd1cde
Michael Ritsch <office@michael-ritsch.com>
2024-09-27 13:50:29 +0200

    fix: remove ip blocking

--- web/app/plugins/wps-dashboard-pdfs/index.php
+++ web/app/plugins/wps-dashboard-pdfs/index.php
@@ -24,63 +24,58 @@ if (!defined('WPINC')) {
 //load user field groups
 require_once 'fieldgroups/user.php';
 
-// debugging by michaelritsch - 26.09.24
-if (in_array( $_SERVER['REMOTE_ADDR'], ['88.116.97.118', '2a02:8388:2809:c880:a82f:50b:bdce:94bc'])){
+add_action('init', function () {
+    add_rewrite_endpoint('rechnungen', EP_ROOT | EP_PAGES);
+});
 
-    add_action('init', function () {
-        add_rewrite_endpoint('rechnungen', EP_ROOT | EP_PAGES);
-    });
+add_filter('woocommerce_account_menu_items', function ($items) {
 
-    add_filter('woocommerce_account_menu_items', function ($items) {
+    $current_user = wp_get_current_user();
 
-        $current_user = wp_get_current_user();
-
-        if($current_user instanceof WP_User){
-
-            $results = get_field('wps_dashboardpdfs_list', 'user_'.$current_user->ID);
-
-            if(!!$results) {
-
-                return array_slice($items, 0, 3, true)
-                    + array(
-                        'rechnungen' => 'Rechnungen'
-                    )
-                    + array_slice($items, 3, count($items), true);
-            }
-        }
-
-        return array_slice($items, 0, 3, true)
-            + array_slice($items, 3, count($items), true);
-    });
-
-    add_action('woocommerce_account_rechnungen_endpoint', function(){
-
-        $current_user = wp_get_current_user();
-
-        if(!$current_user instanceof WP_User){
-            return;
-        }
+    if($current_user instanceof WP_User){
 
         $results = get_field('wps_dashboardpdfs_list', 'user_'.$current_user->ID);
 
+        if(!!$results) {
 
-        if(is_array($results) && count($results) > 0){
-            echo '<section class="article-content"><figure class="wp-block-table dashboard-table"><table>';
-            echo '<thead><tr><th>Rechnungsnummer</th><th style="text-align: left;">Download PDF</th></tr></thead>';
-
-            foreach ($results as $line){
-
-                extract($line);
-                extract($pdf);
-
-                echo '<tr>';
-                echo '<td>' . $invoice . '</td>';
-                echo '<td>' . '<a style="text-decoration: underline;" href="' . $url . '" target="_blank">' . $title . '</a>' . '</td>';
-                echo '</tr>';
-            }
-            echo '</table></figure></section>';
+            return array_slice($items, 0, 3, true)
+                + array(
+                    'rechnungen' => 'Rechnungen'
+                )
+                + array_slice($items, 3, count($items), true);
         }
+    }
 
-    });
+    return array_slice($items, 0, 3, true)
+        + array_slice($items, 3, count($items), true);
+});
 
-}
+add_action('woocommerce_account_rechnungen_endpoint', function(){
+
+    $current_user = wp_get_current_user();
+
+    if(!$current_user instanceof WP_User){
+        return;
+    }
+
+    $results = get_field('wps_dashboardpdfs_list', 'user_'.$current_user->ID);
+
+
+    if(is_array($results) && count($results) > 0){
+        echo '<section class="article-content"><figure class="wp-block-table dashboard-table"><table>';
+        echo '<thead><tr><th>Rechnungsnummer</th><th style="text-align: left;">Download PDF</th></tr></thead>';
+
+        foreach ($results as $line){
+
+            extract($line);
+            extract($pdf);
+
+            echo '<tr>';
+            echo '<td>' . $invoice . '</td>';
+            echo '<td>' . '<a style="text-decoration: underline;" href="' . $url . '" target="_blank">' . $title . '</a>' . '</td>';
+            echo '</tr>';
+        }
+        echo '</table></figure></section>';
+    }
+
+});
